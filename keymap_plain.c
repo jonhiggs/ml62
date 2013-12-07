e81de0b0f66b439ca70fdc7a53293828baa200a3
Jon Higgs <jhiggs@eml.cc>
2013-12-07 16:09:28 +0100

    remove empty keys from the keymap.

--- keymap_plain.c
+++ keymap_plain.c
@@ -17,9 +17,9 @@
 const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* 0: plain */
     KEYMAP(ESC, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS, EQL, BSLS, GRV,\
-           TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC, RBRC, BSPC, NO,\
-           LCTL,A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT, ENT,    NO, NO,\
-           LSFT,NO,  Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,  NO, UP,  NO,  \
-           FN2,LALT,LGUI,FN1, SPC,      NO,NO,NO,NO,NO,NO,NO,          LEFT,DOWN,RIGHT),
+           TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC, RBRC, BSPC,    \
+           LCTL,A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT, ENT,           \
+           LSFT,     Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,      UP,       \
+           FN2,LALT,LGUI,FN1, SPC,                                     LEFT,DOWN,RIGHT),
 };
 const uint16_t PROGMEM fn_actions[] = {};
