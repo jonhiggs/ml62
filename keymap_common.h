d8af7e9f39007fc22958c1e642e045811ff3c557
Jon Higgs <jhiggs@eml.cc>
2013-12-12 20:24:16 +0100

    Update the matrix identifying the row/column relationship to keys.

--- keymap_common.h
+++ keymap_common.h
@@ -40,11 +40,28 @@ extern const uint16_t fn_actions[];
     K30,    K31, K32, K33, K34, K35, K36, K37, K38, K39, K3A,        K3B,     \
     K40, K41, K42, K43,       K44,                              K4C, K4D, K4E \
 ) { \
-    { KC_##K00, KC_##K01, KC_##K02, KC_##K03, KC_##K04, KC_##K05, KC_##K06, KC_##K07, KC_##K08, KC_##K09, KC_##K0A, KC_##K0B, KC_##K0C, KC_##K0D, KC_##K0E}, \
-    { KC_##K10, KC_##K11, KC_##K12, KC_##K13, KC_##K14, KC_##K15, KC_##K16, KC_##K17, KC_##K18, KC_##K19, KC_##K1A, KC_##K1B, KC_##K1C, KC_##K1D, KC_NO   }, \
-    { KC_##K20, KC_##K21, KC_##K22, KC_##K23, KC_##K24, KC_##K25, KC_##K26, KC_##K27, KC_##K28, KC_##K29, KC_##K2A, KC_##K2B, KC_##K2C, KC_NO   , KC_NO   }, \
-    { KC_##K30, KC_##K31, KC_##K32, KC_##K33, KC_##K34, KC_##K35, KC_##K36, KC_##K37, KC_##K38, KC_##K39, KC_##K3A, KC_##K3B, KC_NO   , KC_NO   , KC_NO   }, \
-    { KC_##K40, KC_##K41, KC_##K42, KC_##K43, KC_##K44, KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_##K4C, KC_##K4D, KC_##K4E}  \
+    { KC_##K11, KC_##K12, KC_##K13, KC_##K14, KC_##K17, KC_##K18, KC_##K19, KC_NO,    KC_NO,    KC_NO    }, \
+    { KC_##K10, KC_##K20, KC_NO,    KC_##K15, KC_##K16, KC_##K0D, KC_##K42, KC_##K1B, KC_##K30, KC_##K0E }, \
+    { KC_##K21, KC_##K22, KC_##K23, KC_##K24, KC_##K27, KC_##K28, KC_##K29, KC_##K2A, KC_##K43, KC_##K1D }, \
+    { KC_##K00, KC_NO,    KC_NO,    KC_##K25, KC_##K26, KC_NO,    KC_NO,    KC_##K2B, KC_##K42, KC_##K44 }, \
+    { KC_##K31, KC_##K32, KC_##K33, KC_##K34, KC_##K37, KC_##K38, KC_##K39, KC_NO,    KC_NO,    KC_##K2C }, \
+    { KC_##K40, KC_NO,    KC_NO,    KC_##K05, KC_##K06, KC_##K0C, KC_NO,    KC_##K0B, KC_NO,    KC_NO    }, \
+    { KC_##K01, KC_##K02, KC_##K03, KC_##K04, KC_##K07, KC_##K08, KC_##K09, KC_##K0A, KC_NO,    KC_NO    }  \
 }
 
 #endif
+
+/*
+
+| Row    | C0 + C1  | C2        | C3 | C4 | C5 | C6 + C14      | C7 + OC13 | C8            | C9 + C12 + C15 | C10 + C11   |
+|--------|----------|-----------|----|----|----|---------------|-----------|---------------|----------------|-------------|
+| **R0** | Q        | W         | E  | R  | U  | I             | O         | P             |                |             |
+| **R1** | TAB      | CONTROL_L |    | T  | Y  | SLASH         | ALT       | BRACKET_LEFT  | SHIFT_L        | BACKQUOTE   |
+| **R2** | A        | S         | D  | F  | J  | K             | L         | SEMICOLON     | FN             | DELETE      |
+| **R3** | ESCAPE   |           |    | G  | H  |               |           | QUOTE         | COMMAND        | SPACE       |
+| **R4** | Z        | X         | C  | V  | M  | COMMA         | DOT       |               |                | RETURN      |
+| **R5** |          |           |    | B  | N  | CURSOR_LEFT   |           | FORWARD_SLASH | CURSOR_RIGHT   | CURSOR_DOWN |
+| **R6** | CAPSLOCK |           |    | 5  | 6  | EQUAL         |           | MINUS         |                |             |
+| **R7** | 1        | 2         | 3  | 4  | 7  | 8             | 9         | 0             |                |             |
+
+*/
