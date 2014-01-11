6a51ae87df7d1a2485cc7cfcf26706422c2bf439
Jon Higgs <jhiggs@eml.cc>
2014-01-11 13:20:58 +0100

    testing what ACTION_MODS_KEY does.

--- keymap_plain.c
+++ keymap_plain.c
@@ -22,7 +22,8 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * Fn action definition
  */
 const uint16_t PROGMEM fn_actions[] = {
-    [0] = ACTION_LAYER_MOMENTARY(1),      // FN0 switch to layer 1
+    [0] = ACTION_LAYER_MOMENTARY(1),          // FN0 switch to layer 1
+    [1] = ACTION_MODS_KEY(MOD_LSFT, KC_9),
 };
 
 
