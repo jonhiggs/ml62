170bc8fe46ba5946f2ef0c1847df585ac494188c
Jon Higgs <jhiggs@eml.cc>
2014-01-11 13:23:27 +0100

    9 already does something.

--- keymap_plain.c
+++ keymap_plain.c
@@ -23,7 +23,7 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 const uint16_t PROGMEM fn_actions[] = {
     [0] = ACTION_LAYER_MOMENTARY(1),          // FN0 switch to layer 1
-    [1] = ACTION_MODS_KEY(MOD_LSFT, KC_9),
+    [1] = ACTION_MODS_KEY(MOD_LSFT, KC_Q),
 };
 
 
