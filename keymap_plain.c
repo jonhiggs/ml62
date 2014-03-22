f9fd4ffa3e4c976fe62b25206e54e4caf269a9e1
Jon Higgs <jhiggs@eml.cc>
2014-03-22 21:23:18 +0100

    try holding the keys on while buttons pressed.

--- keymap_plain.c
+++ keymap_plain.c
@@ -54,14 +54,14 @@ void action_function(keyrecord_t *record, uint8_t id, uint8_t opt)
         if (get_mods(MOD_BIT(KC_LSHIFT))) {
           add_key(KC_GRV);
           send_keyboard_report();
-          del_key(KC_GRV);
-          send_keyboard_report();
         } else {
           add_key(KC_ESC);
           send_keyboard_report();
-          del_key(KC_ESC);
-          send_keyboard_report();
         }
+      } else {
+        del_key(KC_GRV);
+        del_key(KC_ESC);
+        send_keyboard_report();
       }
       break;
   }
