f733f68eb953e8b0dc4de247089cdfffa13b1877
Jon Higgs <jhiggs@eml.cc>
2014-03-22 21:59:53 +0100

    add volume keys.

--- keymap_plain.c
+++ keymap_plain.c
@@ -20,10 +20,10 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       NO,  NO,  NO,  FN0, NO,  NO,  NO,  NO,  NO                                \
     ),
     KEYMAP(   // LAYER 2: Mouse
-      NO,NO,NO,NO, NO,NO, NO,  MPRV,MPLY,MNXT,NO,NO,NO,NO,NO, \
-      NO,NO,NO,NO, NO,NO, NO,  BTN1,BTN3,BTN2,NO,NO,NO,NO,    \
-      NO,NO,NO,NO, NO,NO, MS_L,MS_D,MS_U,MS_R,NO,NO,NO,       \
-      NO,NO,NO,NO, NO,NO, NO,  NO,  NO,  NO,  NO,NO,          \
+      NO,NO,NO,NO, NO,NO, NO,  MPRV,MPLY,MNXT,MUTE,VOLD,VOLU,NO,NO, \
+      NO,NO,NO,NO, NO,NO, NO,  BTN1,BTN3,BTN2,NO,  NO,  NO,  NO,    \
+      NO,NO,NO,NO, NO,NO, MS_L,MS_D,MS_U,MS_R,NO,  NO,  NO,       \
+      NO,NO,NO,NO, NO,NO, NO,  NO,  NO,  NO,  NO,  NO,          \
       NO,NO,NO,FN0,NO,FN1,NO,  NO,  NO                        \
     ),
 };
