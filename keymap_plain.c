358ff1da85fc07c6362982e4c92b1246cfe2b654
Jon Higgs <jhiggs@eml.cc>
2014-03-22 22:18:19 +0100

    oops.

--- keymap_plain.c
+++ keymap_plain.c
@@ -77,7 +77,7 @@ void action_function(keyrecord_t *record, uint8_t id, uint8_t opt)
           del_mods(MOD_BIT(KC_LCTRL));
           add_key(KC_SPC);
           send_keyboard_report();
-         else {
+         } else {
           add_key(KC_SPC);
           send_keyboard_report();
         }
