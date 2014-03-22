d9729a04c1e961df9bdd6d49591e4bd4f33aef1e
Jon Higgs <jhiggs@eml.cc>
2014-03-22 20:51:15 +0100

    try to deal with shift esc.

--- keymap_plain.c
+++ keymap_plain.c
@@ -87,13 +87,17 @@ void action_function(keyrecord_t *record, uint8_t id, uint8_t opt)
       // LShft + tap '('
       // NOTE: cant use register_code to avoid conflicting with magic key bind
       if (event.pressed) {
-        //add_mods(MOD_BIT(KC_LSHIFT));
-        add_mods(MOD_BIT(KC_LSHIFT));
-        add_key(KC_ESC);
-        send_keyboard_report();
-        del_mods(MOD_BIT(KC_LSHIFT));
-        del_key(KC_ESC);
-        send_keyboard_report();
+        if (MOD_BIT(KC_LSHIFT)) {
+          add_key(KC_GRV);
+          send_keyboard_report();
+          del_key(KC_GRV);
+          send_keyboard_report();
+        } else {
+          add_key(KC_ESC);
+          send_keyboard_report();
+          del_key(KC_ESC);
+          send_keyboard_report();
+        }
       }
       break;
   }
