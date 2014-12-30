7569fd5156fb35ce1a2a96ec874a7bf497069c01
Jon Higgs <jhiggs@eml.cc>
2014-12-30 20:20:23 +1100

    Add the to the keymap.

--- keymap_plain.c
+++ keymap_plain.c
@@ -7,21 +7,24 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC, RBRC, FN16,    \
       LCTL,FN14,S,   D,   F,   G,FN13,   J,   K,   L,   SCLN,QUOT, ENT,           \
       LSFT,     Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,      UP,       \
-      FN15,FN2,FN1,LGUI,    FN11,     LALT,                        LEFT,DOWN,RGHT
+      FN15,FN2,FN1,LGUI,    FN11,     LALT,                        LEFT,DOWN,RGHT,\
+                   LGUI
     ),
     KEYMAP(   // LAYER 1: Function1
       TRNS,F1  ,F2  ,F3  ,F4  ,F5  ,F6  ,F7  ,F8  ,F9  ,F10 ,F11 ,F12 , SYSREQ,  NO, \
       TRNS,NO,  NO,  END ,NO,  NO,  NO,  PGUP,NO,  NO,  NO,  NO,  NO,  DELETE,  \
       TRNS,HOME,NO,  PGDN,NO,  NO,  LEFT,DOWN,UP  ,RGHT,NO,  NO,  NO,           \
       TRNS,NO,  NO,  NO,  NO,  NO,  NO,  NO,  NO,  NO,  NO,  NO,                \
-      TRNS,TRNS,TRNS,TRNS,SPC ,NO,  NO,  NO,  NO                                \
+      TRNS,TRNS,TRNS,TRNS,SPC ,NO,  NO,  NO,  NO,                               \
+                     TRNS
     ),
     KEYMAP(   // LAYER 2: Function2
       NO  ,F14,F15,NO, NO,NO, NO,  MPRV,MPLY,MNXT,MUTE,VOLD,VOLU,NO,NO, \
-      NO  ,NO,NO,NO, NO,NO, NO,  NO,  NO,  NO,  NO,  NO,  NO,  NO,    \
-      TRNS,A, NO,NO, NO,NO, H,   NO,  NO,  NO,  NO,  NO,  NO,       \
-      CAPS,NO,NO,NO, NO,NO, NO,  NO,  NO,  NO,  NO,  NO,          \
-      TRNS,FN2,FN1,TRNS,TRNS,TRNS,NO,  NO,  NO                        \
+      NO  ,NO,NO,NO, NO,NO, NO,  NO,  NO,  NO,  NO,  NO,  NO,  NO,      \
+      TRNS,A, NO,NO, NO,NO, H,   NO,  NO,  NO,  NO,  NO,  NO,           \
+      CAPS,NO,NO,NO, NO,NO, NO,  NO,  NO,  NO,  NO,  NO,                \
+      TRNS,FN2,FN1,TRNS,TRNS,TRNS,NO,  NO,  NO,                         \
+                   TRNS                                                 \
     ),
 };
 
